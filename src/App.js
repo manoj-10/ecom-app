6432695e69b24542e8d58b57871922813bc66a97
unknown <pmanojkumar5040@gmail.com>
2022-07-16 15:25:46 +0530

    My first vesion of App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div style={{display:'flex', alignItems:'center', justifyContent:'center'}}>
+      <h1><b>Welcome to the first page!!</b></h1>
+    </div>
+  );
+}
+
+export default App;
